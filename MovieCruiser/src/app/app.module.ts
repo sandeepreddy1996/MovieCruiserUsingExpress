8ae48d00552ad35813b205994b63f4fc482a35d4
sandeepreddy1996 <sandeepkuppili39@gmail.com>
2017-10-17 12:40:19 +0530

    first commit

--- MovieCruiser/src/app/app.module.ts
+++ MovieCruiser/src/app/app.module.ts
@@ -3,13 +3,22 @@ import { SearchBarService } from './search-bar.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule} from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import { WatchlistComponent } from './watchlist/watchlist.component';
+import { WatchlistComponent, MyDialog } from './watchlist/watchlist.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+
+import {MatToolbarModule} from '@angular/material';
+import {MatInputModule} from '@angular/material';
+import {MatButtonToggleModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material';
+import {MatCardModule} from '@angular/material';
+import {MatGridListModule} from '@angular/material';
+import {MatDialogModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -18,12 +27,22 @@ import { MovieDetailComponent } from './movie-detail/movie-detail.component';
     HomeComponent,
     WatchlistComponent,
     MovieDetailComponent,
+    MyDialog
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonToggleModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatDialogModule
   ],
+  entryComponents:[MyDialog],
   providers: [SearchBarService, FavoriteService],
   bootstrap: [AppComponent]
 })
--- MovieCruiser/src/app/watchlist/watchlist.component.ts
+++ MovieCruiser/src/app/watchlist/watchlist.component.ts
@@ -1,6 +1,8 @@
 import { Router,RouterModule } from '@angular/router';
 import { Http, HttpModule } from '@angular/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+
 import 'rxjs/Rx';
 
 @Component({
@@ -14,7 +16,8 @@ export class WatchlistComponent implements OnInit {
   id;
 
   constructor(private http: Http,
-              private router: Router) { }
+              private router: Router, 
+              public dialog: MatDialog) {}
 
   ngOnInit() {
     this.http.get(`http://localhost:3000/watchlist`).subscribe(res => {
@@ -28,26 +31,18 @@ export class WatchlistComponent implements OnInit {
   removeFromFav(id){
     this.http.delete('http://localhost:3000/watchlist/' + String(id)).subscribe(res => {});
     //console.log('http://localhost:3000/watchlist/' + String(id));
-
     let deleted = this.favorites.find((element) => {
       return id == element['id'];
-    });
-    
+    });  
     let index = this.favorites.indexOf(deleted);
-
     this.favorites.splice(index,1);
-
     console.log("modified favorites", this.favorites)
   }
 
   updateComments(newComment){
-
     console.log(this.id);
-
     const obj={"userComments": newComment};
-
     this.http.put('http://localhost:3000/watchlist/'+String(this.id),obj).subscribe(res => {});
-
     this.favorites.forEach((element) => {
       if(this.id == element.id){
         element["userComments"] = newComment;
@@ -59,7 +54,38 @@ export class WatchlistComponent implements OnInit {
     this.id = id;
   }
 
+  openDialog(): void {
+    let dialogRef = this.dialog.open(MyDialog, {
+      width: '500px',
+      data: { id: this.id }
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+
+    });
+  }
+
+
+}
+
+@Component({
+  selector: 'my-dialog',
+  template:`<h2> New Comment {{data.id}}</h2>
+              <mat-form-field class="example-full-width">
+                <textarea #newComment matInput placeholder="New Comment"></textarea>
+              </mat-form-field>
+              <button mat-raised-button [mat-dialog-close]="data.comment" color = "primary"> Save </button>
+            `
+})
+
+export class MyDialog{
+
+
+  constructor(
+    public dialogRef: MatDialogRef<MyDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    ) { }
 
 
 }
